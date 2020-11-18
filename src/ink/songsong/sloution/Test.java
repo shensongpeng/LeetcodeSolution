6b9f37ac745d5b4702ad723dbcad006ab3c2b8c6
松松爱吃肉 <1054282887@qq.com>
2020-11-18 17:37:58 +0800

    改动

--- src/ink/songsong/sloution/Test.java
+++ src/ink/songsong/sloution/Test.java
@@ -1,15 +1,35 @@
 package ink.songsong.sloution;
 
 public class Test {
+    public boolean isPalindrome(String s) {
+        //
+        int i = 0;
+        int j = s.length()-1;
+        while (i != j) {
+            if (!isLetterOrDigit(s.charAt(i))) {
+                i++;
+                continue;
+            }
+            if (!isLetterOrDigit(s.charAt(j))) {
+                j--;
+                continue;
+            }
+            if (!compare(s.charAt(i++),s.charAt(j--))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean isLetterOrDigit(char c) {
+        return Character.isLetterOrDigit(c);
+    }
+    private boolean compare(char source,char target) {
+        source = Character.toLowerCase(source);
+        target = Character.toLowerCase(target);
+        return source == target;
+    }
+
     public static void main(String[] args) {
-        char c = 'c';
-        char d = '中';
-        char e = '1';
-        c = Character.toLowerCase(c);
-        d = Character.toLowerCase(d);
-        e = Character.toLowerCase(e);
-        System.out.println(Character.isLetter(d));
-        System.out.println(d);
-        System.out.println(e);
+        System.out.println((char)(11+1));
     }
 }
